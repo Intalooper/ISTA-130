88267286d65cdb03f0ab214adef14bc4e0549d37
John D <149196435+Intalooper@users.noreply.github.com>
2024-08-25 23:26:10 -0700

    Update student_dic.py
    *Updated method/class documentation and in-line documentation

--- hmwk_projects/student_dic.py
+++ hmwk_projects/student_dic.py
@@ -1,12 +1,21 @@
+'''
+John Diaz
+Class: ISTA 130
+Professor: Dozal
+Start Date: N/A
+Finish Date: N/A
+'''
+
 class Student:
     '''
-    blueprint for a digital student profile, which includes their name, id_number, gpa, phone, and email. 
+    blueprint for a digital student profile, which includes their name, id_number, gpa, phone, and email 
     '''
     def __init__(self, name, id_number, gpa, phone, email):
         '''
-        initializes each instance of student with several different strings to identify the unique student
-        parameters:
+        Initializes each instance of student with several different strings to identify the unique student
+        Parameters (Type: String):
             all are strings and are self-evident
+        Returns None
         '''
         #all of the instance variables are strings
         self.name = name
@@ -16,15 +25,16 @@ class Student:
         self.email = email 
     def __repr__(self):
         '''
-        string representation for an object of student
-        returns string instance_string
+        String representation for an object of Student
+        Returns: returns 'instance_string' -> (Type: String)
         '''
         instance_string = f'Name: {self.name}\n\nID: {self.id_number}\n\nGPA: {self.gpa}\n\nPhone Number: {self.phone}\n\nEmail: {self.email}'
         return instance_string
     
     def student_dict(self):
         '''
-        Student metthod that creates a dictionary for the instance, where the name parameter is the key and the remaining values are the remaining parameters of the instance
+        Class method creates a dictionary for the istance, where the name instance variable is the key and the remaining values are the remaining instance variables
+        Returns: returns dictionary (Type: Dic)
         '''
         a_dict = {self.name: [self.id_number, self.gpa, self.phone, self.email]}
         
@@ -34,7 +44,8 @@ class Student:
 
 def main():
     '''
-    creates an instance of Students, prints the repr string and the instances student dictionary'''
+    Creates an instance of Students, prints the repr string and the instances student dictionary
+    '''
     john = Student('John', '2839293', '4.0', '6232907937', 'johndiazleal@icloud.com')
     print(john)
     print(john.student_dict())
